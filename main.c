c689652a6a1bad80b60d0488df3874b482fe2155
subinjeong123 <55428469+subinjeong123@users.noreply.github.com>
2019-11-07 16:12:44 +0900

    10-5

--- main.c
+++ main.c
@@ -3,17 +3,26 @@
 
 /* run this program using the console pauser or add your own getch, system("pause") or input loop */
 
-void swap (int x, int y)
+int main(void)
 {
-	int temp;
-	temp = x;
-	x=y;
-	y=temp;
+	int i;
+	int grade[5];
+	int average=0;
+	int *gptr;
+	
+	for(i=0;i<5;i++)
+	{
+		printf("grade[%i] = ",i);
+		scanf("%d", &grade[i]);
+	}
+	gptr = grade;
+	for(i=0;i<5;i++)
+	{
+		printf("grade[%d] = %d\n", i, *(gptr+i));
+		average += *(gptr+i);
+	}
+	printf("\n -- average : %i\n", average/5);
+	
+	return 0;
 }
 
-void main(void) {
-	int a = 3;
-	int b = 5;
-	swap(a, b);
-	printf("a:%i, b:%i\n", a, b);
-}
